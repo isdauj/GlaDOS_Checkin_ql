8f166dd3b936be1061b08257df8403ddc8906faa
isdauj <136306576+isdauj@users.noreply.github.com>
2023-09-14 13:30:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 Cookies = [
-    #'koa:sess=XXXXXXXXXXXXXXXXXXXX; koa:sess.sig=XXXXXXXXXX;', #cookie1
-    #'koa:sess=XXXXXXXXXXXXXXXXXXXX; koa:sess.sig=XXXXXXXXXX;', #cookie2
+    #'koa:sess=eyJ1c2VySWQiOjIyNzgzNywiX2V4cGlyZSI6MTcxMjQxMDA3ODc2MSwiX21heEFnZSI6MjU5MjAwMDAwMDB9'; #cookie1
+    #'koa:sess.sig=HndNBknpnQpR3LBpSkBp3PwfX1w; __stripe_mid=49777480-e7d3-449b-96ae-26ead54231ee8339af ', #cookie2
     ]
